69fa0672b806052bc64a69852d8db7bd9c3f16bf
Hayes Geldmacher <42843199+HayesGeldmacher@users.noreply.github.com>
2026-04-19 23:12:37 -0600

    Setting up mult-material support  -HG

--- MeshSplitExperiments/Assets/Scripts/MeshSliceScaffolding.cs
+++ MeshSplitExperiments/Assets/Scripts/MeshSliceScaffolding.cs
@@ -3,22 +3,29 @@ using UnityEngine;
 public class MeshSliceScaffolding : MonoBehaviour
 {
 
+    [Header("Slicing")]
     [SerializeField]
     private MeshFilter _meshFilter;
     [SerializeField]
     private Vector3 _origin;
     [SerializeField]
     private Vector3 _normal;
+
+    [Header("Debug")]
     [SerializeField]
     private bool _drawDebug = false;
 
     [Header("Physics")]
     [SerializeField]
     private bool _addPhysics = true;
-    [SerializeField]
-    private bool _addForce = true;
     [Range(0.0f, 1000.0f)]
     public float sliceForce = 200.0f;
+
+    [Header("Materials")]
+    [SerializeField]
+    private bool useDifferentMat = false;
+    [SerializeField] private Material mat1;
+    [SerializeField] private Material mat2;
 
     private bool isPlaying = false; //are we playing or in-editorr?
 
@@ -37,26 +44,39 @@ public class MeshSliceScaffolding : MonoBehaviour
             GameObject submesh = Instantiate(this.gameObject);
             if (!isPlaying) { submesh.transform.position += submesh.transform.right * 2; }
             submesh.GetComponent<MeshFilter>().sharedMesh = mesh;
-            if(submesh.TryGetComponent<MeshCollider>(out MeshCollider collider))
+
+            if(submesh.transform.TryGetComponent<MeshCollider>(out MeshCollider collider))
             {
                 if (isPlaying) { Destroy(collider); }
                 else { DestroyImmediate(collider); }
             }
-            if(submesh.TryGetComponent<Rigidbody>(out Rigidbody rb))
-            {
-                if (isPlaying) { Destroy(rb); }
-                else { DestroyImmediate(rb); }
-            }
 
-            if (_addPhysics)
+            if (!_addPhysics)
+            {
+                if(submesh.transform.TryGetComponent<Rigidbody>(out Rigidbody rb))
+                {
+                    Debug.Log("got rigidbody for: " + submesh.name);
+                    if (isPlaying) { Destroy(rb); }
+                    else { DestroyImmediate(rb); }
+                }
+            }
+            else
             {
                 MeshCollider newCollider = submesh.AddComponent(typeof(MeshCollider)) as MeshCollider;
                 newCollider.sharedMesh = mesh;
                 newCollider.convex = true;
 
-                Rigidbody rigid = submesh.AddComponent(typeof(Rigidbody)) as Rigidbody;
+                Rigidbody rigid;
+                if (submesh.transform.TryGetComponent<Rigidbody>(out Rigidbody rb))
+                {
+                    rigid = rb;
+                }
+                else
+                {
+                   rigid = submesh.AddComponent(typeof(Rigidbody)) as Rigidbody;
+                }
 
-                if (_addForce && isPlaying)
+                if (sliceForce > 0 && isPlaying)
                 {
                     Vector3 trueCenter = submesh.transform.TransformPoint(mesh.bounds.center);
                     Vector3 targetDir = (transform.position - trueCenter) * -1;
@@ -78,22 +98,10 @@ public class MeshSliceScaffolding : MonoBehaviour
         Gizmos.matrix = Matrix4x4.TRS(transform.position, Quaternion.LookRotation(_normal), Vector3.one);
 
         //draw cubes to represent the slicing plane
+        Vector3 scale = transform.localScale;
         Gizmos.color = new Color(0, 1, 0, 04f);
-        Gizmos.DrawCube(_origin, new Vector3(2, 2, 0.01f));
+        Gizmos.DrawCube(_origin, new Vector3(2 * scale.x, 2 * scale.y, 0.01f * scale.z));
         Gizmos.color = new Color(0, 1, 0, 1f);
-        Gizmos.DrawWireCube(_origin, new Vector3(2, 2, 0.01f));
-
-        //set matrix to object matrix and draw all normals
-        Gizmos.color = Color.blue;
-        Gizmos.matrix = transform.localToWorldMatrix;
-
-        if(_meshFilter == null) { return; }
-        for(int i = 0; i < _meshFilter.sharedMesh.normals.Length; i++)
-        {
-            Vector3 normal = _meshFilter.sharedMesh.normals[i];
-            Vector3 vertex = _meshFilter.sharedMesh.vertices[i];
-            Gizmos.DrawLine(vertex, vertex +  normal);
-        }
-
+        Gizmos.DrawWireCube(_origin, new Vector3(2 * scale.x, 2 * scale.y, 0.01f * scale.z));
     }
 }
